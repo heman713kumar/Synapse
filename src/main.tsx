ca633bd035732fed26d3187085b7cae1f1466e0c
heman713kumar <heman713kumar@gmail.com>
2025-10-25 14:19:04 +0100

    fix(frontend): import main css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
+// C:\Users\hemant\Downloads\synapse\src\main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'; // ← ADD THIS LINE
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
